a723685e239660a1bf6d1e7139b701f6ae8de96a
Connor Gordon <con.gordon@gmail.com>
2019-01-03 14:08:25 -0800

    initial commit

---
+++ src/Albums.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import {} from "./db"
+
+class Albums extends Component {
+    render (){
+
+        return(
+            <Router>
+                <div id="list">
+                    <h3 id="h3">My Albums</h3>
+                sup
+                </div>
+          </Router>
+
+        );
+    }
+}
+
+export default Albums;
---
+++ src/Pictures.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+
+class Pictures extends Component {
+    render () {
+        return (
+          <Router>
+              eyy
+          </Router>  
+        );
+    }
+}
+
+export default Pictures;
